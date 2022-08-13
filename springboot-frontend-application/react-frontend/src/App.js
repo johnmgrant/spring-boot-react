0bcd925398c124c7b33c0afce795835085f24468
John Grant <john.grant@accuweather.com>
2022-08-13 16:10:24 -0400

    additional frontend react dev progress

---
+++ springboot-frontend-application/react-frontend/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    </div>
+  );
+}
+
+export default App;
---
+++ springboot-frontend-application/react-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class ListEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            employees: []
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h2 className='text-center'>Employees List</h2>
+                <div className='row'>
+                    <table className='table table-striped table-boarded'>
+                        <thead>
+                            <tr>
+                                <th>Employee First Name</th>
+                                <th>Employee Last Name</th>
+                                <th>Employee Email ID</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {
+                                this.state.employees.map(
+                                    employee =>
+                                    <tr key={employee.id}>
+                                        <td>{employee.firstName}</td>
+                                        <td>{employee.lastName}</td>
+                                        <td>{employee.emailId}</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ListEmployeeComponent;
