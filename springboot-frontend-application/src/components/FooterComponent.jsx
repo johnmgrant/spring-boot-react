9ef3c0cb1aced857cadbe49eb2674a3709f0a920
John Grant <john.grant@accuweather.com>
2022-08-14 17:34:24 -0400

    - Restructure frontend application directory - Update vscode workspace structure

---
+++ springboot-frontend-application/src/components/FooterComponent.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class FooterComponent extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default FooterComponent;
---
+++ springboot-frontend-application/src/components/HeaderComponent.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class HeaderComponent extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className='navbar navbar'
+                </header>
+            </div>
+        );
+    }
+}
+
+export default HeaderComponent;
